8a619cf180dc93007f884c857c7b353a7b45dde9
1borha <borha2003@gmail.com>
2023-02-12 16:08:55 +0700

    test x2

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './globalStyles/root.scss'
-import CButton from './UI-Kit/button/CButton'
-import CButtonGroup from './UI-Kit/buttonGroup/CButtonGroup'
+// import CButton from './UI-Kit/button/CButton'
+// import CButtonGroup from './UI-Kit/buttonGroup/CButtonGroup'
 import CCheckbox from './UI-Kit/checkbox/CCheckbox'
 
 /**
@@ -10,20 +10,6 @@ import CCheckbox from './UI-Kit/checkbox/CCheckbox'
 const App = (): JSX.Element => {
   return (
     <div style={{ width: '1900px', height: '100px' }}>
-      <CButtonGroup>
-        <CButton onClick={() => console.log(1)} switchType="F" disabled={false}>
-          Deed it well
-        </CButton>
-        <CButton onClick={() => console.log(1)} switchType="C" disabled={false}>
-          Deed
-        </CButton>
-        <CButton onClick={() => console.log(1)} switchType="A" disabled={false}>
-          Deed
-        </CButton>
-        <CButton onClick={() => console.log(1)} switchType="H" disabled={false}>
-          Deed
-        </CButton>
-      </CButtonGroup>
       <CCheckbox onClick={() => console.log(1)} text={'hello'} value={true} />
     </div>
   )
